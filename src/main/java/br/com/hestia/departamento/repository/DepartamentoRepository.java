3558e7ec76f879a1690dc5098ba26402de6601e5
toffan0 <luccastoffano@gmail.com>
2026-08-27 11:35:04 -0300

    Definir regras de validação do Depto

---
+++ src/main/java/br/com/hestia/departamento/repository/DepartamentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.hestia.departamento.repository;
+
+import br.com.hestia.departamento.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository
+        extends JpaRepository<Departamento, Long> {
+}
